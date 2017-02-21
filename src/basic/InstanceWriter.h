8255993c53df0728c933d056f2f8cc642be40c16
YuNan <510727367@qq.com>
2017-02-21 10:06:51 +0800

    write answer

--- src/basic/InstanceWriter.h
+++ src/basic/InstanceWriter.h
@@ -18,11 +18,22 @@ public:
 
 	  const string &label = pInstance->m_label;
 
-	  m_outf << label << "\t";
-	  vector<string> words = pInstance->m_segs;
+	  m_outf << label << " ";
+	  const vector<string> words = pInstance->m_segs;
 	  int word_size = words.size();
 	  for (int idx = 0; idx < word_size; idx++)
 		  m_outf << words[idx] << " ";
+		int att_num = pInstance->m_attributes.size();
+		const vector<string> atts = pInstance->m_attributes;
+		for(int idx = 0; idx < att_num; idx++) {
+			m_outf << atts[idx] << " ";
+		}
+		int eval_num = pInstance->m_evalutions.size();
+		const vector<string> evals = pInstance->m_evalutions;
+		for(int idx = 0; idx < eval_num; idx++) {
+			m_outf << evals[idx] << " ";
+		}
+		m_outf << pInstance->m_polarity << endl;
 	  m_outf << endl;
 	  return 0;
 	}
